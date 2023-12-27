93fb556e408d024349dbc96e0b8e0f063852f884
Amit-priyu <priyuocta@gmail.com>
2023-12-27 23:13:05 +0530

    cc

---
+++ currency-convertor/app.js
@@ -0,0 +1,9 @@
+const burl="https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/jpy.json";
+
+
+const dropdowns=document.querySelectorAll(".dropown select");
+
+// selecting differenct countriess..
+for(code in countrylist ){
+    console.log(code,countrylist[code]);
+}
